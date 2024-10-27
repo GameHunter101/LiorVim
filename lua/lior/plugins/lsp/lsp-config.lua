d6754ce9bb1ba89bc2f32b5505b36fe2ca654877
GameHunter101 <liorscarmeli@gmail.com>
2024-10-27 12:21:55 -0400

    C LSP support

--- lua/lior/plugins/lsp/lsp-config.lua
+++ lua/lior/plugins/lsp/lsp-config.lua
@@ -1,4 +1,4 @@
-return {
+--[[ return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
@@ -26,8 +26,17 @@ return {
                     "tsserver",
                     "html",
                     "tailwindcss",
-                    "lua_ls",
-                    "pyright"
+                    lua_ls = {
+                        settings = {
+                            Lua = {
+                                copmletion = {
+                                    callSnippet = "Replace",
+                                },
+                            },
+                        },
+                    },
+                    "pyright",
+                    clangd = {},
                 },
                 handlers = {
                     function(server_name)
@@ -113,4 +122,88 @@ return {
         end
 
     }
+} ]]
+return {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' },             -- Required
+        { 'williamboman/mason.nvim' },           -- Optional
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },     -- Required
+        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+        { 'L3MON4D3/LuaSnip' },     -- Required
+        { "rafamadriz/friendly-snippets" },
+        { "antosha417/nvim-lsp-file-operations", config = true },
+    },
+    config = function()
+        local lsp = require("lsp-zero").preset({})
+
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({ buffer = bufnr })
+        end)
+
+        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+        lsp.ensure_installed({
+            "codelldb",
+            "clangd",
+            "tsserver",
+        })
+
+        lsp.configure("clangd", {
+            cmd = {
+                "C:\\Users\\liors\\scoop\\apps\\mingw-winlibs-llvm\\current\\bin\\clangd",
+                "--fallback-style=chromium"
+            }
+        })
+
+        lsp.setup()
+
+        local cmp = require("cmp")
+
+        cmp.setup({
+            mapping = {
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+                { name = "buffer" },
+            },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end
+            }
+        })
+
+        local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+
+        vim.diagnostic.config({
+            virtual_text = true,
+            update_in_insert = true,
+            signs = true,
+            severity_sort = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
 }
