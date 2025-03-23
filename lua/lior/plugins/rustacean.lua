8a986ce0739e7f728cfc2ce05c91ad14b12be65e
GameHunter101 <liorscarmeli@gmail.com>
2025-03-23 11:20:13 -0400

    Disabled clippy for rust

--- lua/lior/plugins/rustacean.lua
+++ lua/lior/plugins/rustacean.lua
@@ -2,4 +2,16 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5",
     lazy = false,
+    config = function()
+        --[[ require("rustaceanvim").setup({
+            tools = {
+                enable_clippy = false,
+            }
+        }) ]]
+        vim.g.rustaceanvim = {
+            tools = {
+                enable_clippy = false,
+            }
+        }
+    end
 }
